4b7bbe2a0daf8be08dbd3e230ffd0907d5246899
Paolo Belforte <pbelforte2003@gmail.com>
2022-01-04 12:02:21 -0300

    make it better

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$l+)yvtw!1f5cyv@5p+_2sjg%ox53lv9hlcpavwzre&_q2qxod
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'gestionstockbelforte.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'gestionstockbelforte.herokuapp.com', '*']
 
 
 # Application definition
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = '/ProyectoWebApp/static/'
+STATIC_ROOT = 'ProyectoWebApp/static'
 
 STATIC_URL = '/static/'
 
